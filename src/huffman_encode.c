2dfa167bfc0a10fd34b934a24ee911fdd0b77f42
ZhangYuke1220 <sonodateitoku@gmail.com>
2020-12-29 00:39:01 +0900

    updated count_symbles and build_tree

--- src/huffman_encode.c
+++ src/huffman_encode.c
@@ -3,13 +3,9 @@
 #include <limits.h>
 #include "huffman_encode.h"
 
-// 各シンボルの数を数える為にポインタを定義
-// 数を数える為に、1byteの上限+1で設定しておく
 static const int nsymbols = 256 + 1;
-//int symbol_count[nsymbols];
 int *symbol_count;
 
-// ノードを表す構造体
 typedef struct node
 {
     int symbol;
@@ -18,26 +14,13 @@ typedef struct node
     struct node *right;
 } Node;
 
-// このソースで有効なstatic関数のプロトタイプ宣言
-// 一方で、ヘッダファイルは外部からの参照を許す関数の宣言のみ
 
-// ファイルを読み込み、static配列の値を更新する関数
 static void count_symbols(const char *filename);
-
-// 与えられた引数でNode構造体を作成し、そのアドレスを返す関数
 static Node *create_node(int symbol, int count, Node *left, Node *right);
-
-// Node構造体へのポインタが並んだ配列から、最小カウントを持つ構造体をポップしてくる関数
-// n は 配列の実効的な長さを格納する変数を指している（popするたびに更新される）
 static Node *pop_min(int *n, Node *nodep[]);
-
-// ハフマン木を構成する関数（根となる構造体へのポインタを返す）
 static Node *build_tree(void);
-
-// 木を深さ優先で操作する関数
 static void traverse_tree(const int depth, const Node *np);
 
-// 以下 static関数の実装
 static void count_symbols(const char *filename)
 {
     FILE *fp = fopen(filename, "rb");
@@ -71,26 +54,16 @@ static Node *create_node(int symbol, int count, Node *left, Node *right)
 
 static Node *pop_min(int *n, Node *nodep[])
 {
-    // Find the node with the smallest count
-    // カウントが最小のノードを見つけてくる
     int argmin = 0;
     for (int i = 0; i < *n; i++)
-    {
         if (nodep[i]->count < nodep[argmin]->count)
-        {
             argmin = i;
-        }
-    }
 
     Node *node_min = nodep[argmin];
 
-    // Remove the node pointer from nodep[]
-    // 見つかったノード以降の配列を前につめていく
     for (int i = argmin; i < (*n) - 1; i++)
-    {
         nodep[i] = nodep[i + 1];
-    }
-    // 合計ノード数を一つ減らす
+
     (*n)--;
 
     return node_min;
@@ -103,46 +76,39 @@ static Node *build_tree()
 
     for (int i = 0; i < nsymbols; i++)
     {
-        // カウントの存在しなかったシンボルには何もしない
         if (symbol_count[i] == 0)
             continue;
 
-        // 以下は nodep[n++] = create_node(i, symbol_count[i], NULL, NULL); と1行でもよい
         nodep[n] = create_node(i, symbol_count[i], NULL, NULL);
         n++;
     }
 
-    const int dummy = -1; // ダミー用のsymbol を用意しておく
+    const int dummy = -1;
     while (n >= 2)
     {
         Node *node1 = pop_min(&n, nodep);
         Node *node2 = pop_min(&n, nodep);
 
-        // Create a new node
-        // 選ばれた2つのノードを元に統合ノードを新規作成
-        // 作成したノードはnodep にどうすればよいか?
-        /*
-      ???
-    */
+        nodep[n] = create_node(dummy, node1->count + node2->count, node1, node2);
+        n++;
     }
-    // 気にした後は symbol_counts は free
+
     free(symbol_count);
+    printf("%p\n", nodep[0]);
     return (n == 0) ? NULL : nodep[0];
 }
 
-// Perform depth-first traversal of the tree
-// 深さ優先で木を走査する
-// 現状は何もしていない（再帰してたどっているだけ）
 static void traverse_tree(const int depth, const Node *np)
 {
     if (np->left == NULL)
         return;
 
+    
+
     traverse_tree(depth + 1, np->left);
     traverse_tree(depth + 1, np->right);
 }
 
-// この関数のみ外部 (main) で使用される (staticがついていない)
 int encode(const char *filename)
 {
     count_symbols(filename);
